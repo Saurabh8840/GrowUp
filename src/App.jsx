eb705bc481a4e6323d3f019e226ef32a1dbc0066
Saurabh8840 <saurabhofficial8840@gmail.com>
2025-05-15 23:37:25 +0530

    add about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import CompanyLogo from './components/CompanyLogo'
+import './App.css';
+import PurposeSection from './components/PurposeSection';
+import FeaturesSection from './components/FeaturesSection';
+import ScheduleSection from './components/ScheduleSection';
 
 
 function App() {
 
-  return <div>
-    <Navbar />
-  </div>
+  return (
+    <main  className='relative min-h-screen overflow-x-hidden'>
+     <div className='absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500 opacity-20 rounded-full blur-[80px] -z-10' ></div>
+     <div className='overflow-hidden'>
+       <Navbar />
+       <Hero />
+       <CompanyLogo />
+       <PurposeSection />
+       <FeaturesSection />
+       <ScheduleSection />
+     </div>
+     </main>
+     
+  
+    
+)
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
-           <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
+      <nav className='fixed top-0 left-0 right-0  bg-white/90 backdrop-blur-sm z-50 border-b-2 border-gray-100 shadow-sm'>
+           <div className='w-full container  mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16'>
 
             {/* logo */}
             <div className='flex items-center gap-1 coursor-pointer'>
