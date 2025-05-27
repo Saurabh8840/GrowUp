9bf31b4638ee412cd4ac126e79ee63113ab27d5e
Saurabh Tripathi <115134438+Saurabh8840@users.noreply.github.com>
2025-05-27 23:10:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
        <Hero />
        <CompanyLogo />
        <PurposeSection />
+{/*        <FeaturesSection /> */}
        <FeaturesSection />
        <ScheduleSection />
        <MonitorSection />
